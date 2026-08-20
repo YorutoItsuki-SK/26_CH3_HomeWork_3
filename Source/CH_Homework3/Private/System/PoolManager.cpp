b668432af5ac1b8eca1cbd9a05f8bc8f94178acd
이승기 <thouxcv@gmail.com>
2026-08-20 20:37:18 +0900

    추가, 관리자, 풀 관리 및 랜덤 아이템 뽑기를 위한 관리자 추가

---
+++ Source/CH_Homework3/Private/System/PoolManager.cpp
@@ -0,0 +1,69 @@
+﻿#include "System/PoolManager.h"
+#include "DataTables/TableItemSpawn.h"
+#include "System/PoolBase.h"
+
+APoolManager::APoolManager()
+{
+
+}
+
+APoolBase* APoolManager::AddPool(TSubclassOf<APoolingObject> PoolingClass, APoolBase* NewPool)
+{
+	if (PoolsMap.Contains(PoolingClass)) {
+		return PoolsMap[PoolingClass];
+	}
+	PoolsMap.Add(PoolingClass, NewPool);
+	return NewPool;
+}
+
+APoolingObject* APoolManager::GetRandomItem()
+{
+	if (!ItemSpawnTable) return nullptr;
+
+	TArray<FTableItemSpawn*> AllRows;
+	static const FString ContextString(TEXT("APoolManager::GetRandomItem"));
+	ItemSpawnTable->GetAllRows(ContextString, AllRows);
+
+	if (AllRows.IsEmpty()) return nullptr;
+
+	float TotalChance = 0.f;
+	for (const FTableItemSpawn* Row : AllRows) {
+		if (!Row) {
+			continue;
+		}
+		TotalChance += Row->SpawnChance;
+	}
+
+	const float RandValue = FMath::FRandRange(0.f, TotalChance);
+	float StackedChance = 0.f;
+
+	TSubclassOf<AItemBase> SpawnClass = nullptr;
+
+	for (FTableItemSpawn* Row : AllRows) {
+		if (!Row) {
+			continue;
+		}
+		StackedChance += Row->SpawnChance;
+		if (RandValue <= StackedChance) {
+			SpawnClass = Row->ItemClass;
+			break;
+		}
+	}
+
+	if (!SpawnClass) {
+		return nullptr;
+	}
+
+	APoolBase* SelectedPool = PoolsMap[SpawnClass];
+	if (!SelectedPool) {
+		return nullptr;
+	}
+
+	return SelectedPool->Acquire();
+}
+
+void APoolManager::SetItemSpawnTable(UDataTable* NewItemSpawnTable)
+{
+	ItemSpawnTable = NewItemSpawnTable;
+}
+
---
+++ Source/CH_Homework3/Public/System/PoolManager.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PoolManager.generated.h"
+
+class APoolBase;
+class APoolingObject;
+
+UCLASS()
+class CH_HOMEWORK3_API APoolManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APoolManager();
+
+	APoolBase* AddPool(TSubclassOf<APoolingObject> PoolingClass, APoolBase* NewPool);
+
+	UFUNCTION(BlueprintCallable, Category = "Pool")
+	APoolingObject* GetRandomItem();
+	UFUNCTION(BlueprintCallable, Category = "Pool")
+	void SetItemSpawnTable(UDataTable* NewItemSpawnTable);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pool")
+	TMap<TSubclassOf<APoolingObject>, APoolBase*> PoolsMap;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pool")
+	UDataTable* ItemSpawnTable;
+};
