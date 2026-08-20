022cb2a715982613aaf868950ab45ace28b8ef41
이승기 <thouxcv@gmail.com>
2026-08-20 20:34:49 +0900

    추가, 클래스, 아이템을 위한 기본 클래스 추가

---
+++ Source/CH_Homework3/Private/Items/ItemBase.cpp
@@ -0,0 +1,91 @@
+﻿#include "Items/ItemBase.h"
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
+AItemBase::AItemBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	SetRootComponent(Scene);
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(Scene);
+
+	CollisionActive = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionActive"));
+	CollisionActive->SetupAttachment(Scene);
+	CollisionActive->OnComponentBeginOverlap.AddDynamic(this, &AItemBase::OnOverlapStartActive);
+	CollisionActive->OnComponentEndOverlap.AddDynamic(this, &AItemBase::OnOverlapEndActive);
+
+	CollisionUi = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionUi"));
+	CollisionUi->SetupAttachment(Scene);
+	CollisionUi->OnComponentBeginOverlap.AddDynamic(this, &AItemBase::OnOverlapStartWidget);
+	CollisionUi->OnComponentEndOverlap.AddDynamic(this, &AItemBase::OnOverlapEndWidget);
+	CollisionUi->InitSphereRadius(300.f);
+
+	ItemWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("Widget"));
+	ItemWidget->SetVisibility(false);
+	ItemWidget->SetupAttachment(Scene);
+}
+
+void AItemBase::ActivateItem(AActor* Activator)
+{
+	bIsActivated = true;
+}
+
+void AItemBase::Deploy(FVector Location, FRotator Rotate)
+{
+	SetActorLocationAndRotation(Location, Rotate);
+	bIsActivated = false;
+	Super::OnAcquire();
+}
+
+void AItemBase::OnOverlapStartWidget(
+	UPrimitiveComponent* OverlappedComp, 
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, 
+	bool bFromSweep, 
+	const FHitResult& SweepResult)
+{
+	if (!OtherActor || !OtherActor->ActorHasTag("Player")) return;
+
+	if (ItemWidget) {
+		ItemWidget->SetVisibility(true);
+	}
+}
+
+void AItemBase::OnOverlapEndWidget(
+	UPrimitiveComponent* OverlappedComp, 
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex)
+{
+	if (!OtherActor || !OtherActor->ActorHasTag("Player")) return;
+
+	if (ItemWidget) {
+		ItemWidget->SetVisibility(false);
+	}
+}
+
+void AItemBase::OnOverlapStartActive(
+	UPrimitiveComponent* OverlappedComp,
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, 
+	bool bFromSweep, 
+	const FHitResult& SweepResult)
+{
+	if (!OtherActor || !OtherActor->ActorHasTag("Player")) return;
+	if (bIsActivated) return;
+	bIsActivated = true;
+	ActivateItem(OtherActor);
+}
+
+void AItemBase::OnOverlapEndActive(
+	UPrimitiveComponent* OverLappedComp, 
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex)
+{
+}
---
+++ Source/CH_Homework3/Public/Items/ItemBase.h
@@ -0,0 +1,70 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "System/PoolingObject.h"
+#include "ItemBase.generated.h"
+
+class USphereComponent;
+class UWidgetComponent;
+
+UCLASS()
+class CH_HOMEWORK3_API AItemBase : public APoolingObject
+{
+	GENERATED_BODY()
+
+public:
+	AItemBase();
+
+	virtual void ActivateItem(AActor* Activator);
+
+	virtual void Deploy(FVector Location, FRotator Rotate);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Component")
+	USceneComponent* Scene;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Component")
+	UStaticMeshComponent* StaticMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Component")
+	USphereComponent* CollisionActive;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Component")
+	USphereComponent* CollisionUi;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effect")
+	USoundBase* PickupSound;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effect")
+	UParticleSystem* Particle;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effect")
+	UWidgetComponent* ItemWidget;
+
+	bool bIsActivated = false;
+
+	virtual void OnOverlapStartWidget(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+	virtual void OnOverlapEndWidget(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex
+	);
+	virtual void OnOverlapStartActive(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+	virtual void OnOverlapEndActive(
+		UPrimitiveComponent* OverLappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex
+	);
+	
+
+};
