43f719bf06411515876c73331b56479fc49e95ee
이승기 <thouxcv@gmail.com>
2026-08-20 20:35:31 +0900

    추가, 아이템, 점수용 아이템 추가

---
+++ Source/CH_Homework3/Private/Items/ItemCoin.cpp
@@ -0,0 +1,12 @@
+﻿#include "Items/ItemCoin.h"
+
+AItemCoin::AItemCoin()
+{
+	Score = 10;
+}
+
+void AItemCoin::ActivateItem(AActor* Activator)
+{
+	Super::ActivateItem(Activator);
+	Super::ReturnToPool();
+}
---
+++ Source/CH_Homework3/Public/Items/ItemCoin.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/ItemBase.h"
+#include "ItemCoin.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH_HOMEWORK3_API AItemCoin : public AItemBase
+{
+	GENERATED_BODY()
+
+public:
+	AItemCoin();
+	
+protected:
+	int32 Score;
+
+	virtual void ActivateItem(AActor* Activator) override;
+};
