febf0ed8d8497dae717fb1203d327c74502f85fc
이승기 <thouxcv@gmail.com>
2026-08-20 20:35:45 +0900

    추가, 아이템, 회복용 아이템 추가

---
+++ Source/CH_Homework3/Private/Items/ItemHeal.cpp
@@ -0,0 +1,12 @@
+﻿#include "Items/ItemHeal.h"
+
+AItemHeal::AItemHeal()
+{
+	HealAmount = 20;
+}
+
+void AItemHeal::ActivateItem(AActor* Activator)
+{
+	Super::ActivateItem(Activator);
+	Super::ReturnToPool();
+}
---
+++ Source/CH_Homework3/Public/Items/ItemHeal.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/ItemBase.h"
+#include "ItemHeal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH_HOMEWORK3_API AItemHeal : public AItemBase
+{
+	GENERATED_BODY()
+public:
+	AItemHeal();
+protected:
+	UPROPERTY(EditAnywhere, Category = "Item|Heal")
+	int32 HealAmount;
+
+	virtual void ActivateItem(AActor* Activator) override;
+};
