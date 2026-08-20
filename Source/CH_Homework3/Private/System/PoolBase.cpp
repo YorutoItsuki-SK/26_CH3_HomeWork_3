86ccc4f6988e7755bed989fd8add01e22f8df16c
이승기 <thouxcv@gmail.com>
2026-08-20 11:45:18 +0900

    추가, 풀, 풀링 오브젝트를 부모로 삼은 객체들의 풀링을 지원하기 위한 풀 객체 추가

---
+++ Source/CH_Homework3/Private/System/PoolBase.cpp
@@ -0,0 +1,54 @@
+﻿#include "System/PoolBase.h"
+#include "System/PoolingObject.h"
+
+APoolBase::APoolBase()
+{
+	AllObjects = {};
+	AvailableObjects = {};
+	ObjectClassSaved = nullptr;
+}
+
+void APoolBase::InitalizePool(TSubclassOf<APoolingObject> ObjectClass, int32 PoolSize)
+{
+	if (ObjectClassSaved) {
+		return;
+	}
+	ObjectClassSaved = ObjectClass;
+	for (int32 i = 0; i < PoolSize; i++) {
+		APoolingObject* NewObject =
+			GetWorld()->SpawnActor<APoolingObject>(ObjectClass);
+
+		if (NewObject) {
+			AllObjects.Add(NewObject);
+			AvailableObjects.Add(NewObject);
+		}
+	}
+}
+
+APoolingObject* APoolBase::Acquire()
+{
+	if (AvailableObjects.IsEmpty()) {
+		APoolingObject* NewObject =
+			GetWorld()->SpawnActor<APoolingObject>(ObjectClassSaved);
+		AllObjects.Add(NewObject);
+		NewObject->SetPoolState(EPoolingObjectState::Active);
+		return NewObject;
+	}
+	APoolingObject* PopedObject = AvailableObjects.Pop();
+	PopedObject->SetPoolState(EPoolingObjectState::Active);
+	return PopedObject;
+}
+
+void APoolBase::Release(APoolingObject* Object)
+{
+	if (!Object) {
+		return;
+	}
+	if (Object->GetPoolState() == EPoolingObjectState::InPool) {
+		return;
+	}
+	AvailableObjects.Push(Object);
+	Object->SetPoolState(EPoolingObjectState::InPool);
+	Object->OnRelease();
+}
+
---
+++ Source/CH_Homework3/Public/System/PoolBase.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PoolBase.generated.h"
+
+class APoolingObject;
+
+UCLASS()
+class CH_HOMEWORK3_API APoolBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APoolBase();
+
+	void InitalizePool(
+		TSubclassOf<APoolingObject> ObjectClass,
+		int32 PoolSize
+	);
+
+	APoolingObject* Acquire();
+	void Release(APoolingObject* Object);
+
+private:
+	UPROPERTY()
+	TArray<TObjectPtr<APoolingObject>> AllObjects;
+	UPROPERTY()
+	TArray<TObjectPtr<APoolingObject>> AvailableObjects;
+
+	UPROPERTY()
+	TSubclassOf<APoolingObject> ObjectClassSaved;
+};
