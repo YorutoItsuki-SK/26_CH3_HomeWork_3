ccea96a152dbdf23073c78d33e9cff4a6b554865
이승기 <thouxcv@gmail.com>
2026-08-20 12:06:01 +0900

    추가, 빌드, 의존성에 UMG와 GameplayTags 추가

--- Source/CH_Homework3/CH_Homework3.Build.cs
+++ Source/CH_Homework3/CH_Homework3.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -8,7 +8,15 @@ public class CH_Homework3 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+			"UMG",
+			"GameplayTags"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
