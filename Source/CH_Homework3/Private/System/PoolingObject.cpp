e4e9b23b460700d91b745c99998c048ba137a877
이승기 <thouxcv@gmail.com>
2026-08-20 11:44:01 +0900

    추가, 풀링 오브젝트, 풀링 하는 객체의 기본, 부모 클래스 추가

---
+++ Source/CH_Homework3/Private/System/PoolingObject.cpp
@@ -0,0 +1,45 @@
+﻿#include "System/PoolingObject.h"
+#include "System/PoolBase.h"
+
+APoolingObject::APoolingObject()
+{
+	Pool = nullptr;
+	PoolState = EPoolingObjectState::InPool;
+}
+
+void APoolingObject::OnAcquire(bool bNeedTick)
+{
+	SetActorHiddenInGame(false);
+	SetActorEnableCollision(true);
+	SetActorTickEnabled(bNeedTick);
+}
+
+void APoolingObject::OnRelease()
+{
+	SetActorHiddenInGame(true);
+	SetActorEnableCollision(false);
+	SetActorTickEnabled(false);
+}
+
+void APoolingObject::SetPool(APoolBase* InPool)
+{
+	if (!Pool) {
+		return;
+	}
+	Pool = InPool;
+}
+
+void APoolingObject::ReturnToPool() {
+	Pool->Release(this);
+}
+
+EPoolingObjectState APoolingObject::GetPoolState() const
+{
+	return PoolState;
+}
+
+void APoolingObject::SetPoolState(EPoolingObjectState NewState)
+{
+	PoolState = NewState;
+}
+
---
+++ Source/CH_Homework3/Public/System/PoolingObject.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PoolingObject.generated.h"
+
+class APoolBase;
+
+UENUM()
+enum class EPoolingObjectState : uint8
+{
+	InPool,
+	Active
+};
+
+UCLASS()
+class CH_HOMEWORK3_API APoolingObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APoolingObject();
+
+	UFUNCTION(BlueprintCallable, Category = "Pool")
+	virtual void OnAcquire(bool bNeedTick = false);
+	UFUNCTION(BlueprintCallable, Category = "Pool")
+	virtual void OnRelease();
+
+	UFUNCTION(BlueprintCallable, Category = "Pool")
+	void SetPool(APoolBase* InPool);
+	UFUNCTION(BlueprintCallable, Category = "Pool")
+	void ReturnToPool();
+
+	EPoolingObjectState GetPoolState() const;
+
+private:
+	UPROPERTY()
+	TObjectPtr<APoolBase> Pool;
+
+	EPoolingObjectState PoolState;
+
+	void SetPoolState(EPoolingObjectState NewState);
+
+	friend class APoolBase;
+
+};
