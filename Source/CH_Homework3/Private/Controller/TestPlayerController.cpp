b1d6ba76d194ac2dd4ad3c2f2e3be527c7f6c652
이승기 <thouxcv@gmail.com>
2026-08-25 16:41:44 +0900

    추가, 입력 및 컨트롤 추가

---
+++ Source/CH_Homework3/Private/Controller/TestPlayerController.cpp
@@ -0,0 +1,131 @@
+﻿#include "Controller/TestPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "BeltGameState.h"
+#include "BeltGameInstance.h"
+
+ATestPlayerController::ATestPlayerController():
+	InputMappingContext(nullptr),
+	MoveAction(nullptr),
+	LookAction(nullptr),
+	HUDWidgetClass(nullptr),
+	HUDWidgetInstance(nullptr),
+	MainMenuWidgetClass(nullptr),
+	MainMenuWidgetInstance(nullptr)
+{
+}
+
+void ATestPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ULocalPlayer* LocalPlayer = GetLocalPlayer();
+	if (!LocalPlayer) {
+		return;
+	}
+	UEnhancedInputLocalPlayerSubsystem* PlayerSubSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+	if (!PlayerSubSystem) {
+		return;
+	}
+	
+	if (!InputMappingContext) {
+		return;
+	}
+	PlayerSubSystem->AddMappingContext(InputMappingContext, 0);
+
+}
+
+UUserWidget* ATestPlayerController::GetHUDWidget() const
+{
+	return HUDWidgetInstance;
+}
+
+void ATestPlayerController::ShowGameHud()
+{
+	if (HUDWidgetInstance) {
+		HUDWidgetInstance->RemoveFromParent();
+		HUDWidgetInstance = nullptr;
+	}
+
+	if (MainMenuWidgetInstance) {
+		MainMenuWidgetInstance->RemoveFromParent();
+		MainMenuWidgetInstance = nullptr;
+	}
+
+	if (!HUDWidgetClass) {
+		return;
+	}
+	HUDWidgetInstance = CreateWidget<UUserWidget>(this, HUDWidgetClass);
+	if (!HUDWidgetInstance) {
+		return;
+	}
+	HUDWidgetInstance->AddToViewport();
+	bShowMouseCursor = false;
+	SetInputMode(FInputModeGameOnly());
+
+	ABeltGameState* GameState = GetWorld() ? GetWorld()->GetGameState<ABeltGameState>() : nullptr;
+	if (!GameState) {
+		return;
+	}
+	GameState->UpdateHUD();
+}
+
+void ATestPlayerController::ShowMainMenu(bool bIsRestart)
+{
+	if (HUDWidgetInstance) {
+		HUDWidgetInstance->RemoveFromParent();
+		HUDWidgetInstance = nullptr;
+	}
+
+	if (MainMenuWidgetInstance) {
+		MainMenuWidgetInstance->RemoveFromParent();
+		MainMenuWidgetInstance = nullptr;
+	}
+
+	if (!MainMenuWidgetClass) {
+		return;
+	}
+	MainMenuWidgetInstance = CreateWidget<UUserWidget>(this, MainMenuWidgetClass);
+	if (!MainMenuWidgetInstance) {
+		return;
+	}
+	MainMenuWidgetInstance->AddToViewport();
+	bShowMouseCursor = true;
+	SetInputMode(FInputModeUIOnly());
+
+	UTextBlock* ButtonText = Cast<UTextBlock>(MainMenuWidgetInstance->GetWidgetFromName(TEXT("StartButtonText")));
+	if (!ButtonText) {
+		return;
+	}
+	if (bIsRestart) {
+		ButtonText->SetText(FText::FromString(TEXT("Restart")));
+	}
+	else {
+		ButtonText->SetText(FText::FromString(TEXT("Start")));
+	}
+
+	if (bIsRestart) {
+		UFunction* PlayAnimFunc = MainMenuWidgetInstance->FindFunction(FName("PlayGameOverAnim"));
+		if (!PlayAnimFunc) {
+			return;
+		}
+		MainMenuWidgetInstance->ProcessEvent(PlayAnimFunc, nullptr);
+
+		UTextBlock* TotalScoreText = Cast<UTextBlock>(MainMenuWidgetInstance->GetWidgetFromName("TotalScoreText"));
+		if (!TotalScoreText) {
+			return;
+		}
+		UBeltGameInstance* GameInstance = Cast<UBeltGameInstance>(UGameplayStatics::GetGameInstance(this));
+		if (!GameInstance) {
+			return;
+		}
+		TotalScoreText->SetText(
+			FText::FromString(FString::Printf(TEXT("Total Score : %d"), GameInstance->GetScore()))
+		);
+	}
+}
---
+++ Source/CH_Homework3/Public/Controller/TestPlayerController.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TestPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class CH_HOMEWORK3_API ATestPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATestPlayerController();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookAction;
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, Category = "HUD")
+	UUserWidget* GetHUDWidget() const;
+	UFUNCTION(BlueprintCallable, Category = "HUD")
+	void ShowGameHud();
+	UFUNCTION(BlueprintCallable, Category = "Menu")
+	void ShowMainMenu(bool bIsRestart);
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HUD")
+	TSubclassOf<UUserWidget> HUDWidgetClass;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "HUD")
+	UUserWidget* HUDWidgetInstance;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HUD")
+	TSubclassOf<UUserWidget> MainMenuWidgetClass;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "HUD")
+	UUserWidget* MainMenuWidgetInstance;
+};
