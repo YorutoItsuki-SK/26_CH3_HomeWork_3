8edcf7ab915445bbd35d7e209ba982a2e706f3e5
이승기 <thouxcv@gmail.com>
2026-08-25 16:44:27 +0900

    추가, 웨이브 정보 구조체 추가

---
+++ Source/CH_Homework3/Private/DataTables/TableWave.cpp
@@ -0,0 +1,1 @@
+﻿#include "DataTables/TableWave.h"
---
+++ Source/CH_Homework3/Public/DataTables/TableWave.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "TableWave.generated.h"
+
+USTRUCT(BlueprintType)
+struct FTableWave : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float WaveTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 SpawnCount;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float BeltSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpawnFrequency;
+};
