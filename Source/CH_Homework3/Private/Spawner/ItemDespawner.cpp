7430de9d483b1f355abbb7432e0b689105440edf
이승기 <thouxcv@gmail.com>
2026-08-25 16:42:24 +0900

    추가, 아이템 회수용 객체 추가

---
+++ Source/CH_Homework3/Private/Spawner/ItemDespawner.cpp
@@ -0,0 +1,46 @@
+﻿#include "Spawner/ItemDespawner.h"
+
+#include "Components/BoxComponent.h"
+
+#include "Items/ItemBase.h"
+
+// Sets default values
+AItemDespawner::AItemDespawner()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	SetRootComponent(Scene);
+	DeSpawningBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	DeSpawningBox->SetupAttachment(Scene);
+
+}
+
+void AItemDespawner::BeginPlay()
+{
+	Super::BeginPlay();
+	DeSpawningBox->OnComponentBeginOverlap.AddDynamic(this, &AItemDespawner::OnOverlapStart);
+	
+}
+
+void AItemDespawner::DeSpawnItem(AItemBase* TriggeredItem)
+{
+	if (!TriggeredItem) {
+		return;
+	}
+	TriggeredItem->ReturnToPool();
+}
+
+void AItemDespawner::OnOverlapStart(
+	UPrimitiveComponent* OverlappedComp, 
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, 
+	bool bFromSweep, 
+	const FHitResult& SweepResult)
+{
+	if (!OtherActor) {
+		return;
+	}
+	DeSpawnItem(Cast<AItemBase>(OtherActor));
+}
+
---
+++ Source/CH_Homework3/Public/Spawner/ItemDespawner.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemDespawner.generated.h"
+
+class UBoxComponent;
+class AItemBase;
+
+UCLASS()
+class CH_HOMEWORK3_API AItemDespawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItemDespawner();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
+	USceneComponent* Scene;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
+	UBoxComponent* DeSpawningBox;
+
+	UFUNCTION()
+	void DeSpawnItem(AItemBase* TriggeredItem);
+
+	UFUNCTION()
+	virtual void OnOverlapStart(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+};
