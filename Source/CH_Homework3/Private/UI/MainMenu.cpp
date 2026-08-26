40806795f2f30fdea216cb38594bac40514ed3f1
이승기 <thouxcv@gmail.com>
2026-08-27 00:56:26 +0900

    추가, UI, 메인 UI 추가

---
+++ Source/CH_Homework3/Private/UI/MainMenu.cpp
@@ -0,0 +1,100 @@
+﻿#include "UI/MainMenu.h"
+
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/StackBox.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+#include "BeltGameState.h"
+
+void UMainMenu::SetBoxScoreShow(bool bIsShow)
+{
+	if (BoxScore) {
+		if (bIsShow) {
+			BoxScore->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+		else {
+			BoxScore->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	}
+}
+
+void UMainMenu::SetBoxStartShow(bool bIsShow)
+{
+	if (BoxStart) {
+		if (bIsShow) {
+			BoxStart->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+		}
+		else {
+			BoxStart->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	}
+}
+
+void UMainMenu::SetMenuText(FString NewMenuText)
+{
+	if (MenuText) {
+		MenuText->SetText(
+			FText::FromString(
+				NewMenuText
+			)
+		);
+	}
+}
+
+void UMainMenu::SetScoreText(FString NewScoreText)
+{
+	if (ScoreText) {
+		ScoreText->SetText(
+			FText::FromString(
+				NewScoreText
+			)
+		);
+	}
+}
+
+void UMainMenu::NativeConstruct() 
+{
+	Super::NativeConstruct();
+
+	if (ButtonStart) {
+		ButtonStart->OnClicked.AddDynamic(
+			this,
+			&UMainMenu::OnButtonStartClicked
+		);
+	}
+
+	if (ButtonEnd) {
+		ButtonEnd->OnClicked.AddDynamic(
+			this,
+			&UMainMenu::OnButtonEndClicked
+		);
+	}
+}
+
+void UMainMenu::OnButtonStartClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("UMainMenu::OnButtonStartClicked"));
+	ABeltGameState* BeltGameState = Cast<ABeltGameState>(GetWorld()->GetGameState());
+	if (!BeltGameState) {
+		UE_LOG(LogTemp, Warning, TEXT("UMainMenu::OnButtonStartClicked, BeltGameState is Null"));
+		return;
+	}
+	BeltGameState->EndLevel();
+}
+
+void UMainMenu::OnButtonEndClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("UMainMenu::OnButtonEndClicked"));
+	UWorld* World = GetWorld();
+	if (!World) return;
+
+	APlayerController* PlayerController = World->GetFirstPlayerController();
+
+	UKismetSystemLibrary::QuitGame(
+		World,
+		PlayerController,
+		EQuitPreference::Quit,
+		false
+	);
+}
---
+++ Source/CH_Homework3/Public/UI/MainMenu.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+class UButton;
+class UTextBlock;
+class UStackBox;
+
+UCLASS()
+class CH_HOMEWORK3_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+    UFUNCTION()
+    void SetBoxScoreShow(bool bIsShow);
+    UFUNCTION()
+    void SetBoxStartShow(bool bIsShow);
+    UFUNCTION()
+    void SetMenuText(FString NewMenuText);
+    UFUNCTION()
+    void SetScoreText(FString NewScoreText);
+
+protected:
+
+    virtual void NativeConstruct() override;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UStackBox> BoxScore;
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UStackBox> BoxStart;
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UButton> ButtonStart; 
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UButton> ButtonEnd;
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UTextBlock> ScoreText;
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UTextBlock> MenuText;
+
+    UFUNCTION()
+    void OnButtonStartClicked();
+    UFUNCTION()
+    void OnButtonEndClicked();
+};
