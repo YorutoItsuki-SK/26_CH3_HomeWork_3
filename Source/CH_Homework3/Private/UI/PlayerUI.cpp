f27663f4bf39095801ca0df715ccb1693d6d7cfb
이승기 <thouxcv@gmail.com>
2026-08-26 14:56:30 +0900

    추가, UI, 플레이어 상태 확인용 UI 추가

---
+++ Source/CH_Homework3/Private/UI/PlayerUI.cpp
@@ -0,0 +1,37 @@
+﻿#include "UI/PlayerUI.h"
+#include "Components/TextBlock.h"
+#include "Components/ProgressBar.h"
+
+void UPlayerUI::SetScore(int32 Score)
+{
+	if (!TotalScoreText) {
+		return;
+	}
+    TotalScoreText->SetText(
+        FText::FromString(
+            FString::Printf(
+                TEXT("Score : %d"),Score
+            )
+        )
+    );
+}
+
+void UPlayerUI::SetHealth(int32 CurrentHealth, int32 MaxHealth)
+{
+    if (HealthText) {
+        HealthText->SetText(
+            FText::FromString(
+                FString::Printf(
+                    TEXT("%d / %d"), 
+                    CurrentHealth, MaxHealth
+                ) 
+            )
+        );
+    }
+
+    if (HealthBar) {
+        HealthBar->SetPercent(
+            (float)CurrentHealth / (float)MaxHealth
+        );
+    }
+}
---
+++ Source/CH_Homework3/Public/UI/PlayerUI.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerUI.generated.h"
+
+class UTextBlock;
+class UProgressBar;
+
+UCLASS()
+class CH_HOMEWORK3_API UPlayerUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "UI|Score")
+	void SetScore(int32 Score);
+	UFUNCTION(BlueprintCallable, Category = "UI|Health")
+	void SetHealth(int32 CurrentHealth, int32 MaxHealth);
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> HealthText;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> HealthBar;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TotalScoreText;
+};
